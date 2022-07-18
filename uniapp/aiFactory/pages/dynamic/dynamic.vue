5c04e6c6e816a3b9722f391435782721527fefe2
WY008 <zhaojiaojiao@forone-cn.com>
2022-07-18 11:38:44 +0800

    代码提交

---
+++ uniapp/aiFactory/pages/dynamic/dynamic.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">公司</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.text-area {
+	display: flex;
+	justify-content: center;
+}
+
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+</style>
